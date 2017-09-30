3e0a14303e09166fe4a10f5f2ccbda75b1b9f564
atenhave <atenhave@umich.edu>
2017-09-29 21:22:25 -0500

    initial setup

--- app.py
+++ app.py
@@ -3,11 +3,8 @@ import extensions
 import config
 
 # Initialize Flask app with the template folder address
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 
-
-# Listen on external IPs
-# For us, listen to port 3000 so you can just run 'python app.py' to start the server
-if __name__ == '__main__':
-    # listen on external IPs
-    app.run(host=config.env['host'], port=config.env['port'], debug=True)
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
