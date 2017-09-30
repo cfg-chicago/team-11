bb45bded9d49f52a42e7dce46efc207538427df3
atenhave <atenhave@umich.edu>
2017-09-29 22:00:58 -0500

    running app locally

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ import extensions
 import config
 
 # Initialize Flask app with the template folder address
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app.register_blueprint(controllers.main)
+
+
+# Listen on external IPs
+# For us, listen to port 3000 so you can just run 'python app.py' to start the server
+if __name__ == '__main__':
+    # listen on external IPs
+    app.run(host=config.env['host'], port=config.env['port'], debug=True)
