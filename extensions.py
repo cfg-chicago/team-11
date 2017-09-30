73db1ede8d75c2ae320f05e56f9a0a1dbe6a7cb8
atenhave <atenhave@umich.edu>
2017-09-30 02:46:27 -0500

    fixed config db

--- extensions.py
+++ extensions.py
@@ -4,7 +4,7 @@ import config
 
 def connect_to_database():
   options = {
-    'host': '52.90.43.79',
+    'host': config.env['host'],
     'user': config.env['user'],
     'passwd': config.env['password'],
     'db': config.env['db'],
