6c29105176bd2037ac31a0abfe3a11da8f8bfbfc
Keaton Bonds <keatonb@umich.edu>
2017-09-30 11:24:07 -0400

    Nodes for demo

--- static/js/script.js
+++ static/js/script.js
@@ -4,38 +4,64 @@ console.log('Hello Console!');
 var map = {
 	"pathways" : [
 		{
+			"name" : "base",
+			"x" : [0.628],
+			"y" : [0.521]
+		},
+		{
 			"name" : "career",
-			"color" : "#0000FF",
-			"x" : [0.1, 0.6, 0.4],
-			"y" : [0.4, 0.1, 0.3]
+			"x" : [0.627, 0.5795, 0.5795],
+			"y" : [0.425, 0.4, 0.3]
+		},
+		{
+			"name" : "collaboration",
+			"x" : [0.540, 0.463, 0.400],
+			"y" : [0.416, 0.305, 0.190]
+		},
+		{
+			"name" : "college",
+			"x" : [0.47, 0.55, 0.38],
+			"y" : [0.525, 0.5165, 0.515]
+		},
+		{
+			"name" : "change_agency",
+			"x" : [0.648, 0.582, 0.495],
+			"y" : [0.58, 0.65, 0.735]
 		}
 	]
 }
 
 var progress = {
-	"career" : 2
+	"base" : 1,
+	"career" : 3,
+	"collaboration" : 3,
+	"college" : 3,
+	"change_agency" : 3
 }
 
 function loadNodes() {
-	
 	var map_element = document.getElementById("map");
-	var offsetX = map_element.offsetLeft;
-	var width = map_element.offsetWidth;
-	var height = map_element.offsetHeight;
+	var bg_img = document.getElementById("background-img");
+	var offsetX = bg_img.offsetLeft;
+	var width = bg_img.offsetWidth;
+	var height = bg_img.offsetHeight;
 			
-	console.log(offsetX)
+	console.log(height)
 	
 	for (var i = 0, n = map.pathways.length; i < n; i++) {
 		
 		var pathway = map.pathways[i];
 		
-		for(var j = 1, num = progress[pathway.name]; j < num; j++)
+		for(var j = 0, num = progress[pathway.name]; j < num; j++)
 		{
 			var new_node = document.createElement("div");
 			new_node.className = "node";
 			new_node.style.position = "absolute";
 			new_node.style.left = (map.pathways[i].x[j] * width) + offsetX;
 			new_node.style.top = (map.pathways[i].y[j] * height);
+			
+			
+			
 			map_element.appendChild(new_node);
 		}
 	}
