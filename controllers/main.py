44d0ce4215d4131f557c342af609d3ac6ad0cba7
Zihe Xia <zihex@umich.edu>
2017-09-30 06:44:00 -0500

    table name fixed

--- controllers/main.py
+++ controllers/main.py
@@ -32,7 +32,7 @@ def main_hello():
 def check_login(uname, psw):
     db = connect_to_database()
     cur = db.cursor()
-    cur.execute('SELECT Password FROM USERS WHERE Username="%s";', uname)
+    cur.execute('SELECT Password FROM Users WHERE Username="%s";', uname)
     results = cur.fetchall()
     print(results)
 
