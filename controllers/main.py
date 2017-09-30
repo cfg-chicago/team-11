c84178761617db05e49fa6776f94e64ef0af502f
Zihe Xia <zihex@umich.edu>
2017-09-29 23:30:32 -0500

    create table queries added to main

--- controllers/main.py
+++ controllers/main.py
@@ -4,12 +4,15 @@ from extensions import connect_to_database
 
 main = Blueprint('main', __name__, template_folder='templates')
 
+def import_text_file(filename):
+	return open(filename, "r")
 
 @main.route('/')
 def main_hello():
     # db = connect_to_database()
     # cur = db.cursor()
-    # cur.execute('SELECT username, firstname FROM User')
+    # create_table_queries = open("create_tables.sql", "r")
+    # cur.execute(create_table_queries)
     # results = cur.fetchall()
     return 'Hello world'
     #return render_template("index.html")
